4fd538cf2ecf1a9ecc5bbb08d5a377ec7459158e
REHAN SHAH <spamhomeacc123@gmail.com>
2024-08-28 13:23:33 +0530

    Changes in route routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { schoolRouter } from '../router/schoolRouter.js';
+import { schoolRouter } from './router/schoolRouter.js';
 const app=express()
 
 app.use(express.json())
